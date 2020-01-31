641b1efc0f72329d71c21d25cf56632e271a4beb
Placido Wang <placidow@gmail.com>
2020-01-31 21:21:43 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,12 @@
 def using_while
   levitation_force = 6
 	
-	while levitation_force < 10 do
-	  puts "Wingardium Leviosa"
-	  levitation_force += 1
-	end
+# 	while levitation_force < 10 do
+# 	  puts "Wingardium Leviosa"
+# 	  levitation_force += 1
+# 	end
+	
+	while levitation_force < 10 { levitation_force += 1 }
 end
 
 
